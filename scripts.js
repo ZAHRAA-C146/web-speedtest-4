9675214a72ccf70d29dcc20932dfaa1847f5b730
ZAHRAA-C146 <70736050+ZAHRAA-C146@users.noreply.github.com>
2020-10-03 17:27:58 +0300

    DONE

--- scripts.js
+++ scripts.js
@@ -6,3 +6,17 @@ let instructor = {
     numberOfRepos: 200,
   },
 };
+
+//Menawer
+ 
+//JavaScript
+
+//careless10
+
+console.log(instructor.name)
+
+console.log(instructor.courses[0])
+
+console.log(instructor.githubAccount.username)
+alert("استاد مناور اقوى استاد ويب" )
+
